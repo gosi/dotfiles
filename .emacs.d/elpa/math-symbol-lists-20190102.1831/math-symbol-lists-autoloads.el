ca941cca94a5af5c0458d8f105efe08f8118b265
gosi <ericjansson510@gmail.coom>
2019-01-17 23:45:18 +0100

    z

---
+++ .emacs.d/elpa/math-symbol-lists-20190102.1831/math-symbol-lists-autoloads.el
@@ -0,0 +1,16 @@
+;;; math-symbol-lists-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("math-symbol-lists-pkg.el" "math-symbol-lists.el"
+;;;;;;  "msl-build.el") (23616 31959 345647 763000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; math-symbol-lists-autoloads.el ends here
---
+++ .emacs.d/elpa/slime-company-20190115.1601/slime-company-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "slime-company" "20190115.1601" "slime completion backend for company mode" '((emacs "24.4") (slime "2.13") (company "0.9.0")) :commit "c359296ef40f87f046bfbc4549e657c6885a480e" :keywords '("convenience" "lisp" "abbrev") :authors '(("Ole Arndt" . "anwyn@sugarshark.com")) :maintainer '("Ole Arndt" . "anwyn@sugarshark.com"))
