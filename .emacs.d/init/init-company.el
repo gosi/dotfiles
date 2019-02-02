c4911dad320aeb2c3213263bc8ce35f7988536ab
gosi <ericjansson510@gmail.coom>
2019-02-02 17:21:36 +0100

    C-n/C-p selection

--- .emacs.d/init/init-company.el
+++ .emacs.d/init/init-company.el
@@ -10,4 +10,10 @@
 (require-package 'company-jedi)
 (company-auctex-init)
 
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
 (provide 'init-company)
